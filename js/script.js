50314d6803cbc5ac9fbf815abbaa40beca63b7ea
n_meeks <najja.meeks3@gmail.com>
2026-02-09 21:24:17 -0800

    Created variables, placeholder function and click event

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+const word = "magnolia";
+
+const placeholder = function (word) {
+   const placeholderLetters = [];
+   for (const letter of word) {
+    console.log(letter);
+   
+ }
+  wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessLetterButton.addEventListener("click", function (e) {
+   e.preventDefault();
+   const newGuess = letterInput.value;
+   console.log(newGuess);
+   letterInput.value = "";
+});
